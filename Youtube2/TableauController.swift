2cb9a33dcf20d7db42b69d024f6f9b20214ede70
Kevin Trebossen <kevintrebossen@host.dynamic.voo.be>
2018-09-08 15:58:03 +0200

    Ajout du Table View Controller

---
+++ Youtube2/TableauController.swift
@@ -0,0 +1,24 @@
+//
+//  TableauController.swift
+//  Youtube2
+//
+//  Created by Kevin Trebossen on 8/09/18.
+//  Copyright © 2018 KTD. All rights reserved.
+//
+
+import UIKit
+
+class TableauController: UIViewController {
+
+    @IBOutlet weak var TableView: UITableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
